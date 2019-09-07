663ad2e51abbc1dd4193a044b05c065b20b414c6
Giuseppe <giuseppe.dagostino94@gmail.com>
2019-09-07 11:03:29 +0200

    .

--- Main.java
+++ Main.java
@@ -1,42 +1,10 @@
-import targ.*;
 import con.*;
-import java.util.ArrayList;
-import java.util.Scanner;
-
 class Main{
-    public static void main(String[] args) throws Exception { 
-            //Inzializo l'oggetto targhe e la lista che conterrà il risultato dell'algoritmo per l'individuazione della targa
-            Targhe targheObj = new Targhe();
-            ArrayList<String> listaTarghe = new ArrayList<String>();
-            String targaSelezionata;
-            String p = "/home/one/Desktop/test/imgs/su.jpg";// http://193.45.228.92/mjpg/video.mjpg Video o immagine contenenti la targa
-            int operazione;
-            String old=null; //vecchia targa in caso si voglia aggiornarne il valore
-            //Selezione targa
-            targheObj.getTarghe(p);
-            listaTarghe=targheObj.getTargheList();
-            System.out.println(listaTarghe);
-            targheObj.getNumb();
-            targaSelezionata = targheObj.getTargaCustom();
-   
-            //Selezione operazione
-            Scanner keyboard = new Scanner(System.in);
-            System.out.println("Seleziona l'operazione da effettuare");
-            System.out.println("Digita 1 per cercare la targa");
-            System.out.println("Digita 2 per inserire la targa");
-            System.out.println("Digita 3 per aggiornare la targa");
-            System.out.println("Digita 4 per eliminare la targa");
-            operazione = keyboard.nextInt();
+    public static void main(String[] args) { 
+        try{
             Query q = new Query();
-            q.op(operazione,targaSelezionata);
-            if(operazione == 1 ||operazione == 2 ||operazione == 4 ){
-                System.out.println("Operazione effettuata sulla targa: "+q.getRisultato());
-            }else if(operazione == 3){
-                Scanner kb = new Scanner(System.in);
-                System.out.println("hai selezionato l'opzione di aggiorna");
-                System.out.println("inserisci la targa inserita precdentemente");
-                old= kb.nextLine();
-                q.op(operazione, old, targaSelezionata);
-            }
+            q.initQ("/home/one/Desktop/test/imgs/su.jpg");
+        }
+        catch(Exception e){ System.out.println(e);}
     }
 }    
--- con/Query.java
+++ con/Query.java
@@ -1,18 +1,55 @@
 package con;
 import java.sql.*;
+import targ.*;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Query{  
     private String risultato;
+    private Targhe targheObj = new Targhe();
 
     public String getRisultato(){
         return this.risultato;
     }
-    
     public void setRisultato(String par){
         this.risultato = par;
     }
 
-    public String cercaTarga(String param){  //op =1
+    public void initQ(String path) throws Exception{
+            ArrayList<String> listaTarghe = new ArrayList<String>();
+            String targaSelezionata;
+            String p = path;                                                                                                         // http://193.45.228.92/mjpg/video.mjpg Video o immagine contenenti la targa
+            int operazione;
+            String old=null;                                                                                                                                                                    //vecchia targa in caso si voglia aggiornarne il valore
+            //Selezione targa
+            this.targheObj.getTarghe(p);
+            listaTarghe=targheObj.getTargheList();
+            System.out.println(listaTarghe);
+            this.targheObj.getNumb();
+            targaSelezionata = this.targheObj.getTargaCustom();
+            //Selezione operazione
+            Scanner keyboard = new Scanner(System.in);
+            System.out.println("Seleziona l'operazione da effettuare");
+            System.out.println("Digita 1 per cercare la targa");
+            System.out.println("Digita 2 per inserire la targa");
+            System.out.println("Digita 3 per aggiornare la targa");
+            System.out.println("Digita 4 per eliminare la targa");
+            operazione = keyboard.nextInt();
+            this.op(operazione,targaSelezionata);
+            if(operazione == 1 ||operazione == 2 ||operazione == 4 ){
+                if(this.getRisultato()!= null){
+                System.out.println("Operazione effettuata sulla targa: "+this.getRisultato());
+                }
+            }else if(operazione == 3){
+                Scanner kb = new Scanner(System.in);
+                System.out.println("hai selezionato l'opzione di aggiorna");
+                System.out.println("inserisci la targa inserita precdentemente");
+                old= kb.nextLine();
+                this.op(operazione, old, targaSelezionata);
+            }
+    }
+
+    public void cercaTarga(String param){  //op =1
         Statement stmt = null;
         String query = "SELECT * from Targhe where descrizione = '"+param+"'";
         try{
@@ -22,16 +59,19 @@ public class Query{
             "jdbc:mysql://remotemysql.com:3306/CGdY7lHCsM","CGdY7lHCsM","73wAAF3djl");
             stmt=con.createStatement();
             ResultSet rs=stmt.executeQuery(query);
-            while(rs.next()){
-                setRisultato(rs.getString(2));
+            if(rs.next()){
+                rs.beforeFirst();
+                while(rs.next()){
+                    String res = rs.getString(2);
+                        this.setRisultato(res);
+                }
+            } else {
+                System.out.println("Targa non trovata");
             }
             con.close();
         }
         catch(Exception e){ System.out.println(e);}
-        String risultato = getRisultato();
-        return risultato;
     }
-
     public void insTarga(String param){  //op = 2
         Statement stmt = null;
         String query = "INSERT INTO Targhe (descrizione) VALUES ('"+param+"')";
@@ -43,10 +83,10 @@ public class Query{
             stmt=con.createStatement();
             stmt.executeUpdate(query);
             System.out.println("Targa inserita correttamente!");
+            this.setRisultato(param);
         }
         catch(Exception e){ System.out.println("Errore nell'inserimento della targa errore:"+e);}
     }
-
     public void upTarga(String old, String newOne){  //op = 3
         Statement stmt = null;
         String query = "UPDATE Targhe set descrizione = '"+newOne+"' where descrizione = '"+old+"'";
@@ -61,7 +101,6 @@ public class Query{
         }
         catch(Exception e){ System.out.println(e);}
     }
-
     public void delTarga(String param){ //op = 4
         Statement stmt = null;
         String query = "DELETE from Targhe WHERE descrizione = '"+param+"'";  
@@ -73,10 +112,11 @@ public class Query{
             stmt=con.createStatement();
             stmt.executeUpdate(query);
             System.out.println("Targa:'"+param+"' rimossa correttamente!");
+            this.setRisultato(param);
         }
         catch(Exception e){ System.out.println("Erorre nella rimozione della targa:"+e);}
     }
-
+    
     public void op(int num, String param){
         switch(num) {
             case 1:
